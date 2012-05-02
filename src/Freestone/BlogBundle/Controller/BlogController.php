5a2d51098bbc7501cee4902ebf7fcbfa238b2774
Liviu Panainte <liviu.panainte@gmail.com>
2012-05-02 16:03:51 +0100

    Finished part3 of the tutorial. Implemented the Blog entity

---
+++ src/Freestone/BlogBundle/Controller/BlogController.php
@@ -0,0 +1,26 @@
+<?php
+
+/**
+ *
+ * PHP version  5.3
+ * @author      Liviu Panainte <liviu.panainte at gmail.com>
+ */
+
+namespace Freestone\BlogBundle\Controller;
+
+use Symfony\Bundle\FrameworkBundle\Controller\Controller;
+
+class BlogController extends Controller {
+    
+    public function showAction($id) {
+        $em = $this->getDoctrine()->getEntityManager();
+        $blog = $em->getRepository('FreestoneBlogBundle:Blog')->find($id);
+
+        if (!$blog) {
+            throw $this->createNotFoundException('Unable to find Blog post.');
+        }
+
+        return $this->render('FreestoneBlogBundle:Blog:show.html.twig', compact('blog'));
+    }
+    
+}
